7bebc553a8e1228a5b5ae0800f65a8f1624eb184
Admin <Admin@DESKTOP-DFAIJ8K>
2024-12-30 12:00:38 +0200

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,23 @@
+﻿new Solution().FizzBuzz(3);
+public class Solution
+{
+	public IList<string> FizzBuzz(int n)
+	{
+		var list = new List<string>(n);
+
+		for (int i = 1; i <= n; i++)
+		{
+			string item = (i % 3 == 0, i % 5 == 0) switch
+			{
+				(true, true) => "FizzBuzz",
+				(true, false) => "Fizz",
+				(false, true) => "Buzz",
+				(false, false) => i.ToString()
+			};
+
+			list.Add(item);
+		}
+
+		return list;
+	}
+}
